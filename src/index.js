82e0f566f2df522ac39449a1882e3b50a71051d4
Plohocky Artom <plohoc@gmail.com>
2022-01-09 19:10:59 +0200

    feat add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var result = '';
+    while(Math.abs(n)>0){
+    result = result + (Math.abs(n)%10);
+    n = parseInt(Math.abs(n)/10);
+    }
+    return result;
+} 
+
